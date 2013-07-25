29e2430f182be8464db8bc5942eb821aba8bb81a
Jakub Suder <jakub.suder@gmail.com>
2013-07-25 19:32:36 +0200

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+
   protect_from_forgery
+
+  def not_found
+    head :not_found
+  end
 end
